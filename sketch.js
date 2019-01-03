48c632d972122323d5d5d3d0846268162122fcff
carlos lopez <scarloshernan@gmail.com>
2019-01-03 10:37:37 -0500

    se ajusta proyecto
    ajuste a proyecto p5.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,47 @@
 
 var count = 0; // initialize a counter variable
+var tamn = 100;
 
 function setup() {
-  createCanvas(800, 300);
-  textAlign(CENTER, CENTER);
-  frameRate(6);
-
+  createCanvas(450, 450);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
-  fill(150, 34, 93);
-  textSize(36);
-  // get the current frame rate as an integer.
-  text("frameCount: " + frameCount, width/2, height/2);
+	background(1, 186, 240);
+	// declaration of variables
+	var x = width / 2;
+	var y = height / 2;
+	// increment the size with the current frameCount value
+	
+	console.log(tamn);
+
+	if(count === 0){
+
+		if(tamn < 400){
+
+			tamn++;
+
+			if(tamn === 400){count = 400;}
+
+		}
+
+	}
+
+	if(count === 400){
+
+		tamn--;
+
+		if(tamn === 100){count = 0;}		
+
+	}
+
+	// circle
+	fill(237, 34, 93);
+	noStroke();
+	ellipse(x, y, tamn, tamn);
+	// rectangle
+	fill(255);
+	rect(x, y, tamn*0.75, tamn*0.15);
 }
 
